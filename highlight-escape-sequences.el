72c3cdcf7a652d18971ed9a65f30da69deab19a7
Dmitry Gutov <dgutov@yandex.ru>
2013-06-01 02:12:45 +0400

    Change wording

--- highlight-escape-sequences.el
+++ highlight-escape-sequences.el
@@ -22,13 +22,13 @@
 
 ;;; Commentary:
 
-;; This global minor mode highlights escape sequences in string
-;; literals with `font-lock-regexp-grouping-backslash' face when
-;; appropriate.
+;; This global minor mode highlights escape sequences in strings and
+;; other kinds of literals with `font-lock-regexp-grouping-backslash'
+;; face when appropriate.
 
 ;; It currently supports `ruby-mode' and both main JavaScript modes.
 
-;; To enable it in other modes, customize `hes-simple-modes'.
+;; To enable it elsewhere, customize `hes-simple-modes'.
 
 ;; Put this in the init file:
 ;;
